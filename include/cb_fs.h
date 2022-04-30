a9b4295dc3d96ee4d5e9434180fc6ce237b0f78e
Adil Benhlal <abenhlal@cborgdb.com>
2022-05-01 00:59:32 +0200

    feat(fs): add open and close

--- include/cb_fs.h
+++ include/cb_fs.h
@@ -6,8 +6,6 @@
 #ifndef _CB_FS_H
 #define _CB_FS_H
 
-#include <stdlib.h>
-
 int cb_fs_mkdir(const char *path);
 
 int cb_fs_rmdir(const char *dir_path);
@@ -19,5 +17,9 @@ int cb_fs_remove(const char *path);
 int cb_fs_dir_exists(const char *path);
 
 int cb_fs_file_exists(const char *path);
+
+int cb_fs_open(const char *path);
+
+int cb_fs_close(int fd);
 
 #endif // _CB_FS_H
