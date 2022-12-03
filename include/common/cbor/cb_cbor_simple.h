69b91435648846120aea8fab7718784b0337fec2
Adil Benhlal <abenhlal@cborgdb.com>
2022-12-03 19:01:25 +0100

    feat(cbor): add encode false, true, undefined and break

--- include/common/cbor/cb_cbor_simple.h
+++ include/common/cbor/cb_cbor_simple.h
@@ -13,6 +13,14 @@
 // ENCODING //
 //////////////
 
+size_t cb_cbor_encode_false(uint8_t *ev, size_t size);
+
+size_t cb_cbor_encode_true(uint8_t *ev, size_t size);
+
 size_t cb_cbor_encode_null(uint8_t *ev, size_t size);
+
+size_t cb_cbor_encode_undefined(uint8_t *ev, size_t size);
+
+size_t cb_cbor_encode_break(uint8_t *ev, size_t size);
 
 #endif // _CB_CBOR_SIMPLE_H
--- src/common/cbor/cb_cbor_simple.c
+++ src/common/cbor/cb_cbor_simple.c
@@ -5,9 +5,41 @@
 
 #include "common/cbor/cb_cbor_simple.h"
 
+size_t cb_cbor_encode_false(uint8_t *ev, size_t size) {
+  if (size >= 1) {
+    *ev = 0xF4;
+    return 1;
+  }
+  return 0;
+}
+
+size_t cb_cbor_encode_true(uint8_t *ev, size_t size) {
+  if (size >= 1) {
+    *ev = 0xF5;
+    return 1;
+  }
+  return 0;
+}
+
 size_t cb_cbor_encode_null(uint8_t *ev, size_t size) {
   if (size >= 1) {
     *ev = 0xF6;
+    return 1;
+  }
+  return 0;
+}
+
+size_t cb_cbor_encode_undefined(uint8_t *ev, size_t size) {
+  if (size >= 1) {
+    *ev = 0xF7;
+    return 1;
+  }
+  return 0;
+}
+
+size_t cb_cbor_encode_break(uint8_t *ev, size_t size) {
+  if (size >= 1) {
+    *ev = 0xFF;
     return 1;
   }
   return 0;
