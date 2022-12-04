f298f73e3d08dc5c6a6cc0dbf2f8b08f679208f7
Adil Benhlal <abenhlal@cborgdb.com>
2022-12-04 16:24:45 +0100

    test(cbor): cbor_encode_undefined

--- tests/common/cbor/test_cb_cbor_simple.c
+++ tests/common/cbor/test_cb_cbor_simple.c
@@ -130,6 +130,46 @@ void test_cb_cbor_encode_null() {
   assert(memcmp(expect_t2_s2, ev_t2_s2, 2) == 0);
 }
 
+void test_cb_cbor_encode_undefined() {
+  // TEST 1
+  // test with size == 0 (no change)
+  uint8_t ev_t1_s0[2] = {0x00, 0x00}; // encoded value
+  uint8_t expect_t1_s0[2] = {0x00, 0x00}; // [ 0x00, 0x00 ]
+  assert(cb_cbor_encode_undefined(ev_t1_s0, 0) == 0);
+  assert(memcmp(expect_t1_s0, ev_t1_s0, 2) == 0);
+
+  // test with size == 1
+  uint8_t ev_t1_s1[2] = {0x00, 0x00}; // encoded value
+  uint8_t expect_t1_s1[2] = {0xF7, 0x00}; // [ undefined, 0x00 ]
+  assert(cb_cbor_encode_undefined(ev_t1_s1, 1) == 1);
+  assert(memcmp(expect_t1_s1, ev_t1_s1, 2) == 0);
+
+  // test with size == 2
+  uint8_t ev_t1_s2[2] = {0x00, 0x00}; // encoded value
+  uint8_t expect_t1_s2[2] = {0xF7, 0x00}; // [ undefined, 0x00 ]
+  assert(cb_cbor_encode_undefined(ev_t1_s2, 2) == 1);
+  assert(memcmp(expect_t1_s2, ev_t1_s2, 2) == 0);
+
+  // TEST 2 (same test with another end byte)
+  // test with size == 0 (no change)
+  uint8_t ev_t2_s0[2] = {0x00, 0xAB}; // encoded value
+  uint8_t expect_t2_s0[2] = {0x00, 0xAB}; // [ 0x00, 0xAB ]
+  assert(cb_cbor_encode_undefined(ev_t2_s0, 0) == 0);
+  assert(memcmp(expect_t2_s0, ev_t2_s0, 2) == 0);
+
+  // test with size == 1
+  uint8_t ev_t2_s1[2] = {0xFF, 0xAB}; // encoded value
+  uint8_t expect_t2_s1[2] = {0xF7, 0xAB}; // [ undefined, 0xAB ]
+  assert(cb_cbor_encode_undefined(ev_t2_s1, 1) == 1);
+  assert(memcmp(expect_t2_s1, ev_t2_s1, 2) == 0);
+
+  // test with size == 2
+  uint8_t ev_t2_s2[2] = {0xFF, 0xAB}; // encoded value
+  uint8_t expect_t2_s2[2] = {0xF7, 0xAB}; // [ undefined, 0xAB ]
+  assert(cb_cbor_encode_undefined(ev_t2_s2, 2) == 1);
+  assert(memcmp(expect_t2_s2, ev_t2_s2, 2) == 0);
+}
+
 void test_cb_cbor_encode_break() {
   // TEST 1
   // test with size == 0 (no change)
@@ -175,6 +215,7 @@ int main() {
   test_cb_cbor_encode_false();
   test_cb_cbor_encode_true();
   test_cb_cbor_encode_null();
+  test_cb_cbor_encode_undefined();
   test_cb_cbor_encode_break();
 
   return EXIT_SUCCESS;
